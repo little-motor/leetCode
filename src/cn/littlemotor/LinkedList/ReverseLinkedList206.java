c8fe29de2fbf62c08e5499d31ae181ba68f67f98
LittleMotor <479217391@qq.com>
2018-12-20 23:10:12 +0800

    反转链表，用多了一个线性表实现，时间复杂度O（n),空间复杂度O(n)

---
+++ src/cn/littlemotor/LinkedList/ReverseLinkedList206.java
@@ -0,0 +1,47 @@
+package cn.littlemotor.LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ *
+ * @author littlemotor
+ * @date 18.12.20
+ */
+public class ReverseLinkedList206 {
+
+    public class ListNode{
+        int val;
+        ListNode next;
+        public ListNode(int val){
+            this.val = val;
+        }
+    }
+
+    /**
+     * 通过链表转数组的方式进行二次反转,时间复杂度O(n)
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        List<ListNode> list = new ArrayList<ListNode>();
+        while(true) {
+            list.add(head);
+            if(head.next == null) break;
+            head = head.next;
+        }
+        for(int i = list.size() - 1; i > 0; i--){
+            head = list.get(i);
+            head.next = list.get(i - 1);
+            head = head.next;
+            head.next = null;
+        }
+        return list.get(list.size() - 1);
+    }
+}
